5bbf3e692b395ddfbb311f3435e18c4e75a81930
Jaylon <jaylonferguson2001@gmail.com>
2026-04-19 23:06:05 -0500

    update

--- obj/Debug/net10.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net10.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e588c9274004a3165a1919536b0f96724e5fd636")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad0d1874faa937128597a49c9d634bdef758e5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
